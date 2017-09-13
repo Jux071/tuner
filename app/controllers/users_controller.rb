0434a32f861ce789abc2f990b4d0aa59b3796bc6
Jux071 <jux071@gmail.com>
2017-09-13 10:30:13 +0200

    user controller changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 		if @user.save
 			log_in @user
 			flash[:success] = 'Registered successfully!'
-		  redirect_to @user
+		  redirect_to root_path
 		else
 			render :new
 		end
